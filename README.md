be291ba350b59d91d16e00f1f3cb48239c70760e
gfaline <gwenfedgar@gmail.com>
2023-11-06 11:53:55 -0500

    Saving progress

--- README.md
+++ README.md
@@ -9,13 +9,16 @@ Identify Recurring Transactions
 - For the scope of this project, periodicity is daily, weekly, monthly, or yearly
   - They can have the same price or similar name
   - +- 30% of time (So in a week, we can be off by a day or two)
-- Normalized Hamming Distance can be used for descriptors
+  - A period must be at least three times
+- Longest common name can be used for descriptors
   - This will notably allow for people to create recurring expenses that are named wildly different terms even at the same cadence
   - I will try to use this to find a common descriptor to name the expense
+  - This also means that if something is recurring but most of the descriptor is different, it will not flag.
 
 To think about:
 - Do all recurring transaction happen at the same cadence: Will company lunch always be 7 days apart, will any weeks be missed
 - What if the price of a subscription changes? Should it be included with the prior lump of recurring or added to a new one, how is that labeled?
   - For the purpose of this, assume the price must be the same
-- Training data is always useful for testing accuracy, but that allows for overfitting. Measuring real world accuracy would likely involve manual labeling or possibly creative data geenration
-- Material Impact may involve asking customers upon signup how much they spend on each period basis. 
+- Training data is always useful for testing accuracy, but that allows for overfitting. Measuring real world accuracy would likely involve manual labeling or possibly creative data generation
+- Material Impact may involve asking customers upon signup how much they spend on each period basis. Then analyzing the difference. There can also be a survey later to try to assess impact or change in recurring expenses
+- 
--- heron.py
+++ heron.py
@@ -1,2 +1,91 @@
 import json
+import datetime
+from scipy.spatial import distance
+from difflib import SequenceMatcher
+# from itertools import pairwise
+
+
+def string_similarity(str_1, str_2):
+    str_1 = list(str_1)
+    str_2 = list(str_2)
+
+    return 1 - distance.hamming(str_1, str_2)
+
+
+def find_common_name(str_1, str_2):
+    match = SequenceMatcher(None, str_1, str_2).find_longest_match(0, len(str_1), 0, len(str_2))
+    return str_1[match.a:match.a + match.size]
+
+
+def periodicity(transactions, names):
+    times = {}
+    groups = {}
+
+    for name in names:
+        for transaction in transactions:
+            if name in transaction["description"]:
+                #date = datetime.datetime(transaction["date"])
+                date = datetime.datetime(int(transaction["date"].split("-")[0]), int(transaction["date"].split("-")[1]), int(transaction["date"].split("-")[2]))
+                if name in times:
+                    times[name].append(date)
+                    groups[name].append(transaction)
+                else:
+                    times[name]=[date]
+                    groups[name]=[transaction]
+    recurring = []
+    for name in times:
+        time_differences = []
+        for i in range(len(times) - 2):
+            time_differences.append((time_differences[i + 1] - time_differences[i]).days)
+
+        daily = all(difference >= 1 and difference < 2 for difference in time_differences)
+        weekly = all(difference >= 5 and difference < 9 for difference in time_differences)
+        monthly = all(difference >= 21 and difference < 39 for difference in time_differences)
+
+        if daily or weekly or monthly:
+            recurring.append(groups[name])
+    print(recurring)
+    print(len(recurring))
+    return recurring
+
+
+def identify_recurring_transactions(transactions):
+    # Identify Names
+    identify_descriptors = {}
+    for item in transactions:
+        # print("Current Transaction Name: ", item['description'])
+        identifiers = list(identify_descriptors.keys())
+        added = False
+        for attempt in identifiers:
+            common_name = find_common_name(item['description'], attempt)
+            if common_name == attempt:
+                # print("Common name is already a key: " + common_name)
+                identify_descriptors[common_name].append(item)
+                added = True
+                break
+            elif len(common_name)/len(item['description']) >= .6:
+                # print("Good Common Name Found: " + common_name)
+                # print(identify_descriptors[attempt])
+                temp = identify_descriptors[attempt]
+                temp.append(item)
+                identify_descriptors[common_name] = temp
+                added = True
+                identify_descriptors.pop(attempt)
+                break
+        if not added:
+            # print("Adding new key " + item['description'])
+            identify_descriptors[item['description']] = [item]
+    # print(identify_descriptors)
+    # print(len(list(identify_descriptors.keys())))
+    events = periodicity(transactions, list(identify_descriptors.keys()))
+
+
+def main():
+    with open('example.json') as f:
+        data = json.load(f)
+    identify_recurring_transactions(data['transactions'])
+
+
+if __name__ == '__main__':
+  main()
 
